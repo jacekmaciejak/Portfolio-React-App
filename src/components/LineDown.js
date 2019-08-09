ecea642b42eb0b9e75bd80a00d58fdba246c446f
Jacek Maciejak <jacekmaciejak@poczta.fm>
2019-08-09 17:46:09 +0200

    Styled comp to LineDown com added

--- src/components/LineDown.js
+++ src/components/LineDown.js
@@ -1,6 +1,38 @@
 import React from "react";
-import "../styles/LineDown.scss";
+import styled from "styled-components";
+
+const Line = styled.div`
+  position: absolute;
+  top: 90%;
+  left: 50%;
+  transform: translate(-90%, -50%);
+  width: 2px;
+  height: 150px;
+  background: transparent;
+  color: #000;
+
+  ::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(transparent, #000, transparent);
+    animation: animateScrollDown 2s linear infinite;
+  }
+
+  @media only screen and (max-width: 1200px) {
+    top: 85%;
+    left: 10%;
+    transform: translate(-85%, -10%);
+  }
+
+  @media only screen and (max-width: 900px) {
+    display: none;
+  }
+`;
 
 export default function LineDown() {
-  return <div className="line_down" />;
+  return <Line />;
 }
