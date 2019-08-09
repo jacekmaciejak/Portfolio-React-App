e9ad85e76c6c690b3365db74f6066c92f330ff2e
Jacek Maciejak <jacekmaciejak@poczta.fm>
2019-08-09 18:41:24 +0200

    Styled comp to Email added -  not finished

--- src/components/Email.js
+++ src/components/Email.js
@@ -1,23 +1,93 @@
 import "../styles/Email.scss";
 import React from "react";
+import styled from "styled-components";
+
+const StyledWrapper = styled.div`
+  display: grid;
+  justify-items: center;
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  height: 100vh;
+  width: 100%;
+  grid-template-columns: 1fr;
+  grid-template-rows: 1fr 1fr 6fr;
+`;
+const Title_1 = styled.h1`
+  font-size: 7vw
+  color: #000;
+   opacity: .1 !important;
+  padding: 10px;
+`;
+const Title_2 = styled.h2`
+  font-size: 5vw;
+  letter-spacing: 3px;
+  background: linear-gradient(90deg, #fff, #000, #fff);
+  background-repeat: no-repeat;
+  background-size: 80%;
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: rgba(255, 255, 255, 0);
+  animation: animateText 3s linear infinite;
+`;
+const Book = styled.section`
+  position: relative;
+  z-index: 9999;
+`;
+const Book__form = styled.div`
+  width: 55vw;
+  height: 100%;
+  padding: 3rem;
+  border-radius: 3px;
+  -webkit-box-shadow: 0 15px 25px rgba(0, 0, 0, 0.5);
+  box-shadow: 0 15px 25px rgba(0, 0, 0, 0.5);
+  background-image: linear -
+      gradient(
+        105deg,
+        rgba(255, 255, 255, 0.5) 0 %,
+        rgba(255, 255, 255, 0.5) 60 %,
+        transparent 50 %
+      ),
+    url("../images/img2.jpg");
+  background-position: center;
+  background-size: cover;
+
+  @media (max-width: 1200px) {
+    width: 95%;
+    padding: 2rem;
+    background-image: linear-gradient(
+        105deg,
+        rgba(255, 255, 255, 0.8) 0%,
+        rgba(255, 255, 255, 0.8) 65%,
+        transparent 50%
+      ),
+      url("../images/img2.jpg");
+  }
+  @media (max-width: 600px) {
+    background-image: linear-gradient(
+        105deg,
+        rgba(255, 255, 255, 0.5) 0%,
+        rgba(255, 255, 255, 0.5) 75%,
+        transparent 50%
+      ),
+      url("../images/img2.jpg");
+  }
+`;
+const Form = styled.form`
+  width: 60%;
+  @media (max-width: 1200px) {
+    width: 70%;
+  }
+`;
 
 export default function Email() {
   return (
-    <div className="email">
-      <h1 className="email__title" data-aos="fade-up">
-        Zachęcam do kontaktu
-      </h1>
-      <h2 className="email__subtitle" data-aos="fade-up">
-        Kontakt
-      </h2>
-      <section class="section-book" id="contact" data-aos="fade-up">
-        <div class="book__form">
-          <form
-            class="form"
-            autocomplete="off"
-            method="post"
-            action="kontakt.php"
-          >
+    <StyledWrapper>
+      <Title_1 data-aos="fade-up">Zachęcam do kontaktu</Title_1>
+      <Title_2 data-aos="fade-up">Kontakt</Title_2>
+      <Book id="contact" data-aos="fade-up">
+        <Book__form>
+          <Form autocomplete="off" method="post" action="kontakt.php">
             <div class="form__group">
               <input
                 type="text"
@@ -60,9 +130,9 @@ export default function Email() {
                 Wyślij &rarr;
               </button>
             </div>
-          </form>
-        </div>
-      </section>
-    </div>
+          </Form>
+        </Book__form>
+      </Book>
+    </StyledWrapper>
   );
 }
