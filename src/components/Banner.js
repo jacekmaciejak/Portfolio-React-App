82fb7b0e00b1528683efdd0701251a2edcf8da7c
Jacek Maciejak <jacekmaciejak@poczta.fm>
2019-08-12 18:41:03 +0200

    Styled component added to Banner comp

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,13 +1,32 @@
 import React from "react";
-import "../styles/Banner.scss";
+import styled from "styled-components";
+
+const StyledWrapper = styled.div`
+  display: inline-block;
+  text-align: left;
+  color: #000;
+  margin-left: 4vw;
+  width: 60%;
+`;
+const Title_1 = styled.h1`
+  font-size: calc(15px + 3.5vw);
+`;
+const Title_2 = styled.h2`
+  font-size: calc(15px + 3.5vw);
+  line-height: 2;
+`;
+const Paragraph = styled.p`
+  line-height: 2;
+  font-size: calc(10px + 1.8vw);
+`;
 
 export default function Banner({ children, title, subtitle, paragraph }) {
   return (
-    <div className="banner">
-      <h1 className="banner__title">{title}</h1>
-      <h2 className="banner__subtitle">{subtitle}</h2>
-      <p className="banner__paragraph">{paragraph}</p>
+    <StyledWrapper>
+      <Title_1>{title}</Title_1>
+      <Title_2>{subtitle}</Title_2>
+      <Paragraph>{paragraph}</Paragraph>
       {children}
-    </div>
+    </StyledWrapper>
   );
 }
